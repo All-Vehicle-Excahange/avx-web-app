16e648f15ddce287903b32936c438d38e5819aab
Adil <adilpatel010@gmail.com>
2026-03-17 15:08:09 +0530

    fix navabar top download section

--- src/components/features/VehiclDetail/SimulerVehicle.jsx
+++ src/components/features/VehiclDetail/SimulerVehicle.jsx
@@ -50,13 +50,7 @@ function SimulerVehicle() {
 
             {/* Cards Grid */}
             <div
-                className="
-          grid
-          grid-cols-1
-          sm:grid-cols-2
-          lg:grid-cols-4
-          gap-6
-        "
+                className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4"
             >
                 {vehicle.map((car) => (
                     <VehicleCard key={car.id} data={car} />
