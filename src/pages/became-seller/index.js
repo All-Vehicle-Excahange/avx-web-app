9336f3fdaa745a8629919aef1ed66dd01c862304
asrarmaknojiya <asrarjabir786@gmail.com>
2026-03-19 12:23:16 +0530

    created how avx works and documentrequired section in became seller page

--- src/pages/became-seller/index.js
+++ src/pages/became-seller/index.js
@@ -3,12 +3,16 @@ import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/FooterLink";
 import FooterLink from "@/components/layout/FooterLink";
 import Hero from "@/components/features/userSeller/Hero";
+import HowAvxWorks from "@/components/features/userSeller/HowAvxWorks";
+import DocumentRequired from "@/components/features/userSeller/DocumentRequired";
 
 function index() {
     return (
         <>
             <Navbar heroMod scrolled />
             <Hero />
+            <HowAvxWorks/>
+            <DocumentRequired/>
 
             <FooterLink />
             <Footer />
