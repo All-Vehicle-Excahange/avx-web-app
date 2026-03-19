2700cf565afe8611e98313ebe2871b820b31298a
Alkama <sunasaraalkama0000@gmail.com>
2026-03-19 12:18:52 +0530

    create the 'InspectionAdvantage' and 'WhoCanSell' section in become-seller page

--- src/pages/became-seller/index.js
+++ src/pages/became-seller/index.js
@@ -3,13 +3,19 @@ import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/FooterLink";
 import FooterLink from "@/components/layout/FooterLink";
 import Hero from "@/components/features/userSeller/Hero";
+import Layout from "@/components/layout/Layout";
+import InspectionAdvantage from "@/components/features/userSeller/InspectionAdvantage";
+import WhoCanSell from "@/components/features/userSeller/WhoCanSell";
 
 function index() {
     return (
         <>
             <Navbar heroMod scrolled />
             <Hero />
-
+            <Layout>
+                <InspectionAdvantage/>
+                <WhoCanSell/>
+            </Layout>
             <FooterLink />
             <Footer />
         </>
