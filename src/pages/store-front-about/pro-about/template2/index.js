c829bcd5c5d5fd405f79e1382330282ad01709e6
Alkama <sunasaraalkama0000@gmail.com>
2026-03-30 12:15:26 +0530

    complete the pro-tier store front about template 2

--- src/pages/store-front-about/pro-about/template2/index.js
+++ src/pages/store-front-about/pro-about/template2/index.js
@@ -1,26 +1,18 @@
-import Service from "@/components/features/store-front-about/pro-about/template2/Service";
-import Stats from "@/components/features/store-front-about/pro-about/template2/Stats";
-import AboutHero from "@/components/features/store-front-about/pro-about/template2/Hero";
-import MissionVision from "@/components/features/store-front-about/pro-about/template2/MissionVision";
-import Footer from "@/components/layout/Footer";
 import FooterLink from "@/components/layout/FooterLink";
 import Layout from "@/components/layout/Layout";
 import Navbar from "@/components/layout/Navbar";
+import AboutPage from "@/components/features/store-front-about/pro-about/template2/Hero";
+import Footer from "@/components/layout/Footer";
 
 function index() {
   return (
     <>
       <Navbar />
       <Layout>
-        <AboutHero/>
-        <MissionVision/>
-      </Layout>
-      <Stats/>
-      <Layout>
-        <Service/>
+        <AboutPage/>
       </Layout>
       <FooterLink />
-      <Footer />
+      <Footer/>
     </>
   );
 }
