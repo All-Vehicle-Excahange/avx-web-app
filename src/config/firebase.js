bdef1ae3f41fd9f051ef6b9d89f0c9426284d30e
Nihal1487 <chaudharynihal09@gmail.com>
2026-07-04 17:04:19 +0530

    Added ENV

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCy35FFJ_eaGPiG-8Ay9-hEJPVQZpmZ0us",
-  authDomain: "reecomm-1.firebaseapp.com",
-  projectId: "reecomm-1",
-  storageBucket: "reecomm-1.firebasestorage.app",
-  messagingSenderId: "627030040807",
-  appId: "1:627030040807:web:462ac919e6dfa9e1fe8242",
-  measurementId: "G-XCV03JNC7M"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
